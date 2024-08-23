17a38545c2304c92a83cec282e936cf0971466af
scrb7b <dimasakula@gmail.com>
2024-08-23 21:17:35 +0300

    delete not usable imports

--- train_model.py
+++ train_model.py
@@ -1,9 +1,4 @@
 import torch
-import torch.nn as nn
-import torch.optim as optim
-from transformers import BertTokenizer, BertModel
-import pandas as pd
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from build_dataset import ToxicCommentsDataset, get_datasets
 from model import model, criterion, optimizer
